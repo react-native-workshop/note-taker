40dc1e1d609a1d49af1311fb7a03709bff58a102
Rahul gaba <rahulgaba16@gmail.com>
2017-10-28 23:56:04 +0530

    Removed redux-actions lib

--- app/redux/actions/index.actions.js
+++ app/redux/actions/index.actions.js
@@ -1,15 +1,12 @@
-import {createAction} from 'redux-actions';
-
 export const SET_TEXT = 'SET_TEXT';
 export const SET_TITLE = 'SET_TITLE';
 
-export const setTitle = createAction(SET_TITLE);
-/* This ^ is equivalent to
-export const setTitle = (payload) => {
-  return {
-    type: SET_TITLE,
-    payload: payload
-  };
-};
-*/
-export const setText = createAction(SET_TEXT);
+export const setTitle = (payload) => ({
+  type: SET_TITLE,
+  payload: payload
+});
+
+export const setText = (payload) => ({
+  type: SET_TEXT,
+  payload: payload
+});
