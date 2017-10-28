f2c12bda70f58fc18b765abd8babcae7e0a230aa
Rahul gaba <rahulgaba16@gmail.com>
2017-10-28 21:21:41 +0530

    chapter-2 ready

--- app/index.js
+++ app/index.js
@@ -7,7 +7,8 @@ import React, { Component } from 'react';
 import Home from './components/Home/Home.component';
 
 class NoteTaker extends Component {
-  render() {
+  render () {
+    const deviceName = 'Pixel'
     return (
       <Home />
     );
